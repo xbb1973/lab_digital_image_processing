fe66a812df18df8cadb9f0b4111f9f9c4509a927
xbb1973 <xiaobanban1973@163.com>
2020-05-29 19:52:32 +0800

    update README

--- graphics.cpp
+++ graphics.cpp
@@ -11,25 +11,26 @@
 #include <opencv2/core/types_c.h>
 #include <opencv2/core/core_c.h>
 #include <opencv2/highgui/highgui.hpp>
-
-
 using namespace std;
 using namespace cv;
 
+// lab1负责调用实验1的内容逻辑
 void lab1();
-
+// 2、灰度图像二值化处理 具体内容：设置并调整阈值对图像进行二值化处理。
 void binaryzation_image(string image_path, int channels_flag, double threshold);
-
+// 3、灰度图像的对数变换 具体内容：设置并调整 r 值对图像进行对数变换。
 void logarithm_transformation_image(string image_path, int channels_flag, double c);
-
+// 4、灰度图像的伽马变换 具体内容：设置并调整γ值对图像进行伽马变换。
 void gama_transformation_image(string image_path, int channels_flag, double c, double gama);
-
+// 5、彩色图像的补色变换 具体内容：对彩色图像进行补色变换。
 void complementary_colour_transformation_image(string image_path);
-
+// 本地测试图片路径
 const string image_path = "/Users/xbb1973/Documents/code/workdir/lab_digital_image/image/";
 
 int main(){
     lab1();
+    // 此处用于测试图片读取路径、逻辑是否正确。
+    // 1、利用 OpenCV 读取图像。 具体内容：用打开 OpenCV 打开图像，并在窗口中显示
     // Mat srcImage = imread(image_path + "1.jpeg", 1);
     // if (!srcImage.data) {
     //    std::cout << "Image not loaded";
@@ -40,14 +41,27 @@ int main(){
     return 0;
 }
 
+/**
+ * 实验 1：图像灰度变换
+ * 1、利用 OpenCV 读取图像。 具体内容：用打开 OpenCV 打开图像，并在窗口中显示
+ * 2、灰度图像二值化处理 具体内容：设置并调整阈值对图像进行二值化处理。
+ * 3、灰度图像的对数变换 具体内容：设置并调整 r 值对图像进行对数变换。
+ * 4、灰度图像的伽马变换 具体内容：设置并调整γ值对图像进行伽马变换。
+ * 5、彩色图像的补色变换 具体内容：对彩色图像进行补色变换。
+ */
 void lab1(){
     string image = image_path + "lena.png";
+    // 2、灰度图像二值化处理 具体内容：设置并调整阈值对图像进行二值化处理。
     binaryzation_image(image, 1, 100);
+    // 3、灰度图像的对数变换 具体内容：设置并调整 r 值对图像进行对数变换。
     logarithm_transformation_image(image, 0, 100);
+    // 4、灰度图像的伽马变换 具体内容：设置并调整γ值对图像进行伽马变换。
     gama_transformation_image(image, 0, 100, 0.4);
+    // 5、彩色图像的补色变换 具体内容：对彩色图像进行补色变换。
     complementary_colour_transformation_image(image);
 }
 
+// 2、灰度图像二值化处理 具体内容：设置并调整阈值对图像进行二值化处理。
 void binaryzation_image(string image_path, int channels_flag, double threshold) {
     Mat src_image = imread(image_path, channels_flag); //flag=0 灰度图单通道，flag>0 3通道
     for (int i = 0; i < src_image.rows; ++i) {
@@ -72,6 +86,7 @@ void binaryzation_image(string image_path, int channels_flag, double threshold) 
     destroyAllWindows();
 }
 
+// 3、灰度图像的对数变换 具体内容：设置并调整 r 值对图像进行对数变换。
 void logarithm_transformation_image(string image_path, int channels_flag, double c) {
     Mat src_image = imread(image_path, channels_flag); //flag=0 灰度图单通道，flag>0 3通道
 //    s = c log (1 + r)
@@ -94,6 +109,7 @@ void logarithm_transformation_image(string image_path, int channels_flag, double
     destroyAllWindows();
 }
 
+// 4、灰度图像的伽马变换 具体内容：设置并调整γ值对图像进行伽马变换。
 void gama_transformation_image(string image_path, int channels_flag, double c, double gama) {
     Mat src_image = imread(image_path, channels_flag); //flag=0 灰度图单通道，flag>0 3通道
 //    s = c r^gama
@@ -116,6 +132,7 @@ void gama_transformation_image(string image_path, int channels_flag, double c, d
     destroyAllWindows();
 }
 
+// 5、彩色图像的补色变换 具体内容：对彩色图像进行补色变换。
 void complementary_colour_transformation_image(string image_path) {
     int channels_flag = 3;
     Mat src_image = imread(image_path, channels_flag); //flag=0 灰度图单通道，flag>0 3通道
@@ -125,7 +142,7 @@ void complementary_colour_transformation_image(string image_path) {
     for (int i = 0; i < src_image.rows; ++i) {
         for (int j = 0; j < src_image.cols; ++j) {
             if(channels_flag == 0){
-//                do nothing;
+               // do nothing;
             } else {
                 auto value = src_image.at<Vec3b>(i, j);
                 for(int i = 0; i < 3; i++){
